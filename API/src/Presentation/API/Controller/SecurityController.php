6535b822d19b9d115c8b9d46249738769704314e
luisd57 <luisrma7@hotmail.com>
2024-10-07 13:13:33 +0200

    Implemented logout and isAuthenticated endpoints

--- API/src/Presentation/API/Controller/SecurityController.php
+++ API/src/Presentation/API/Controller/SecurityController.php
@@ -3,9 +3,10 @@
 namespace App\Presentation\API\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
@@ -16,9 +17,27 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method should not be reached!');
     }
 
-    #[Route('/api/logout', name: 'app_logout', methods: ['POST'])]
-    public function logout(): void
+    #[Route('/api/is-authenticated', name: 'app_is_authenticated', methods: ['GET'])]
+    public function isAuthenticated(Security $security): JsonResponse
     {
-        //intercepted by the logout key on firewall
+        $user = $security->getUser();
+        if ($user) {
+            return new JsonResponse([
+                'authenticated' => true,
+                'user' => [
+                    'email' => $user->getUserIdentifier(),
+                    'roles' => $user->getRoles(),
+                ]
+            ]);
+        }
+
+        return new JsonResponse(['authenticated' => false]);
+    }
+
+    #[Route('/api/logout', name: 'app_logout', methods: ['POST'])]
+    public function logout(): Response
+    {
+        // This method will never be executed
+        throw new \LogicException('This method should never be reached!');
     }
 }
---
+++ API/src/Presentation/Security/ApiLogoutHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Presentation\Security;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
+
+class ApiLogoutHandler implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [LogoutEvent::class => 'onLogout'];
+    }
+
+    public function onLogout(LogoutEvent $event): void
+    {
+        $response = new JsonResponse(['message' => 'Logged out successfully']);
+        $event->setResponse($response);
+    }
+}
